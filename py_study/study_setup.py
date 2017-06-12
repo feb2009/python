c6bb85a2465b306a9cec1528cd2844dbe46eec80
feb2009 <119546855@qq.com>
2017-06-12 19:10:12 +0800

    Default

---
+++ py_study/study_setup.py
@@ -0,0 +1,8 @@
+# -*- coding:UTF-8 -*-
+__author__ = 'FEB'
+
+from distutils.core import setup
+setup(
+    name = "chardet",packages = ["chardet"], version="1.0.2",description = "universal encoding detector",author ="mark"
+    classifiers=[]
+)
